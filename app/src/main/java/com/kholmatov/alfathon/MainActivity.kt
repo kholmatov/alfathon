a53a2b83ae54e57e21b4e752e3087ecc6119b341
Erkin Kholmatov <kholmatov.me@gmail.com>
2024-02-18 23:22:56 -0600

    Add sounds

--- app/src/main/java/com/kholmatov/alfathon/MainActivity.kt
+++ app/src/main/java/com/kholmatov/alfathon/MainActivity.kt
@@ -1,80 +1,101 @@
 package com.kholmatov.alfathon
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import androidx.compose.foundation.BorderStroke
-//import androidx.compose.foundation.border
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-//import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-//import androidx.compose.material3.Button
-//import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-//import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.graphics.RectangleShape
-//import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
-//import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kholmatov.alfathon.ui.theme.AlfathonTheme
 
 class MainActivity : ComponentActivity() {
-    private var textToSpeech: TextToSpeech? = null
+    private val letters = listOf(
+        "ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض",
+        "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي"
+    )
+
+    private val audioFiles = listOf(
+        "alif", "ba", "taa", "tha", "jeem", "ha", "khaa", "dal",
+        "dhal", "raa", "jaa", "seen", "sheen", "saad", "dhaad",
+        "toa", "dhaa", "ain", "ghain", "faa", "qaaf", "kaaf",
+        "laam", "meem", "noon", "haa", "waw", "yaa"
+    )
+
+
+    private var mediaPlayer: MediaPlayer? = null
+
+    private fun getResourceId(audioFileName: String): Int {
+        return resources.getIdentifier(audioFileName, "raw", packageName)
+    }
+
+    private fun checkResourceExistence(resourceName: String): Boolean {
+//        Log.d("MainActivity"," $packageName, $resourceName")
+        val resId = resources.getIdentifier(resourceName, "raw", packageName)
+        return resId != 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AlfathonTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LetterButtons(letters, speakLetter = ::speakLetter, textToSpeech = textToSpeech)
+                    LetterButtons(letters, playLetterSound = ::playLetterSound)
                 }
             }
         }
+    }
 
-        // Initialize TextToSpeech outside of onCreate function
-        textToSpeech = TextToSpeech(this) { status ->
-            // Handle initialization status
+    private fun playLetterSound(letter: String) {
+
+        val audioFileName = audioFiles[letters.indexOf(letter)] //alif.mp3
+//        val audioFileName = "alif.mp3"
+//        if (checkResourceExistence(audioFileName)) {
+//            // Ресурс существует, продолжаем выполнение кода
+//            Log.d("MainActivity", "Resource $audioFileName. OK!")
+//        } else {
+//            // Ресурс не существует
+//            Log.e("MainActivity", "Resource $audioFileName does not exist.")
+//        }
+
+
+
+        mediaPlayer?.stop() // Stop any previous sound
+
+        val resId = getResourceId(audioFileName)
+//        Log.d("MainActivity", "Resource ID for $audioFileName: $resId")
+        if (resId != 0) {
+            mediaPlayer = MediaPlayer.create(this, resId)
+            mediaPlayer?.start()
+        } else {
+            Log.e("MainActivity", "Invalid resource ID for file: $audioFileName")
         }
     }
 
+
     override fun onDestroy() {
         super.onDestroy()
-        textToSpeech?.shutdown()
+        mediaPlayer?.release()
     }
-
-//    private fun speakLetter(letter: String) {
-//        textToSpeech?.speak(letter, TextToSpeech.QUEUE_ADD, null, "letterId")
-//    }
-
-    // ... rest of the code remains the same
-}
-
-// Define a list of 28 Arabic letters
-val letters = listOf(
-    "ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ", "ر", "ز", "س", "ش", "ص", "ض",
-    "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي"
-)
-
-// Define speakLetter function outside MainActivity
-fun speakLetter(letter: String, textToSpeech: TextToSpeech?) {
-    textToSpeech?.speak(letter, TextToSpeech.QUEUE_ADD, null, "letterId")
 }
 
 @Composable
@@ -82,8 +103,7 @@ fun LetterButtons(
     letters: List<String>,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(5.dp),
-    speakLetter: (String, TextToSpeech?) -> Unit,
-    textToSpeech: TextToSpeech?, // Add textToSpeech parameter
+    playLetterSound: (String) -> Unit
 ) {
     val columns = GridCells.Fixed(4)
 
@@ -100,8 +120,13 @@ fun LetterButtons(
                     .fillMaxWidth(0.25f)
                     .aspectRatio(1f)
                     .clickable {
-                        speakLetter(letter, textToSpeech)
+                        playLetterSound(letter)
                     }
+                    .border(
+                        width = 2.dp,
+                        color = Color.DarkGray,
+                        shape = RectangleShape
+                    )
             ) {
                 Text(
                     text = letter,
@@ -113,12 +138,3 @@ fun LetterButtons(
         }
     }
 }
-
-
-//@Preview(showBackground = true)
-//@Composable
-//fun LetterButtonsPreview() {
-//    AlfathonTheme {
-//        LetterButtons(letters = letters, speakLetter = ::speakLetter, textToSpeech = textToSpeech)
-//    }
-//}
